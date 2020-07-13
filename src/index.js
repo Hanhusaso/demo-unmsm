a7c8ff299868f1804ed9c66de551fe006f4e75aa
Hanhusaso <hans.e.huiza.n@gmail.com>
2020-07-13 14:47:45 -0500

    add process.env.PORT

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const indexRoute = require('./routes/');
 
 // settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');

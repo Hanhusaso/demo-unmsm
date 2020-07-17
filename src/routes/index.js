183f994d77a1f6b2a8b6f45f6bf192e4e86aad94
Hanhusaso <hans.e.huiza.n@gmail.com>
2020-07-16 20:48:03 -0500

    change title to UNMSM demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.render('index.html', { title: 'UNMSM Fake' });
+    res.render('index.html', { title: 'UNMSM demo' });
 });
 
 router.get('/contact', (req,res) => {

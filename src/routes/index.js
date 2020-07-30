70fbaa6b8d19f523a3b59d2009c81c0995c47b41
Hanhusaso <hans.e.huiza.n@gmail.com>
2020-07-30 14:19:08 -0500

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+var pool = mysql.createPool({
+    host: 'prueba-unmsm.crj3yvrhuxhz.sa-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'oaci12345',
+    database: 'prueba',
+});
 
 router.get('/', (req,res) => {
-    res.render('index.html', { title: 'UNMSM demo' });
+    res.render('index.html', { title: 'UNMSM home demo' });
 });
 
 router.get('/contact', (req,res) => {
-    res.render('contact.html', { title: 'UNMSM Fake | Contact' });
+    res.render('contact.html', { title: 'UNMSM constact demo' });
+});
+
+router.get('/query', (req,res) => {
+    
+    pool.getConnection(function(err, connection){
+        if (err) {
+            console.log('ERROR');
+        } else {
+            console.log('CONNECTED');
+        }
+        connection.query('SELECT * FROM prueba', function(error, rows, fields){
+            connection.release();
+            if (error) {
+                console.log('ERROR EN EL QUERY');
+            } else {
+                console.log('QUERY EXITOSO');
+                console.log(rows);
+            }
+        })
+    })
+    
+    res.render('query.html', { title: 'UNMSM query demo' });
 });
 
 module.exports = router;
